c8e077ff25953fb3dfb903d375db7f7807401d88
Ignacio Lopez Visedo <ignacio.lopezv@santalucia.es>
2021-02-18 17:23:20 +0100

    chore:temp commit

--- src/main/java/com/santalucia/example/infrastructure/mybatisnformix/config/InformixMyBatisConfig.java
+++ src/main/java/com/santalucia/example/infrastructure/mybatisnformix/config/InformixMyBatisConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
-import com.santalucia.example.infrastructure.config.InfrastructureLayerConfig;
+import com.santalucia.arq.ams.componentes.database.properties.DatasourceConfiguration;
 
 @Configuration
 @MapperScan(basePackages = InformixMyBatisConfig.BASE_PACKAGE,
@@ -31,7 +31,7 @@ public class InformixMyBatisConfig {
 	@Bean(name = SESSION_FACTORY)
 	@Primary
 	public SqlSessionFactory sqlSessionFactoryBean(
-			@Qualifier(InfrastructureLayerConfig.INFORMIX_DATASOURCE) DataSource dataSource) throws Exception {
+			@Qualifier(DatasourceConfiguration.PRIMARY_DATASOURCE) DataSource dataSource) throws Exception {
 
 		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
 		factoryBean.setDataSource(dataSource);
@@ -44,7 +44,7 @@ public class InformixMyBatisConfig {
 	@Bean(name = TRANSACTION_MANAGER)
 	@Primary
 	public DataSourceTransactionManager informixTransactionManager(
-			@Qualifier(InfrastructureLayerConfig.INFORMIX_DATASOURCE) DataSource dataSource) {
+			@Qualifier(DatasourceConfiguration.PRIMARY_DATASOURCE) DataSource dataSource) {
 		return new DataSourceTransactionManager(dataSource);
 	}
 
--- src/main/java/com/santalucia/example/infrastructure/mybatisoracle/config/OracleMyBatisConfig.java
+++ src/main/java/com/santalucia/example/infrastructure/mybatisoracle/config/OracleMyBatisConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
-import com.santalucia.example.infrastructure.config.InfrastructureLayerConfig;
+import com.santalucia.arq.ams.componentes.database.properties.DatasourceConfiguration;
 
 @Configuration
 @MapperScan(basePackages = OracleMyBatisConfig.BASE_PACKAGE,
@@ -31,7 +31,7 @@ public class OracleMyBatisConfig {
 	@Bean(name = SESSION_FACTORY)
 	@Primary
 	public SqlSessionFactory sqlSessionFactoryBean(
-			@Qualifier(InfrastructureLayerConfig.ORACLE_DATASOURCE) DataSource dataSource) throws Exception {
+			@Qualifier(DatasourceConfiguration.SECONDARY_DATASOURCE) DataSource dataSource) throws Exception {
 
 		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
 		factoryBean.setDataSource(dataSource);
@@ -44,7 +44,7 @@ public class OracleMyBatisConfig {
 	@Bean(name = TRANSACTION_MANAGER)
 	@Primary
 	public DataSourceTransactionManager informixTransactionManager(
-			@Qualifier(InfrastructureLayerConfig.ORACLE_DATASOURCE) DataSource dataSource) {
+			@Qualifier(DatasourceConfiguration.SECONDARY_DATASOURCE) DataSource dataSource) {
 		return new DataSourceTransactionManager(dataSource);
 	}
 
