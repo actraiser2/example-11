3e91a243e808e6b336148b6fb54d578f5ea03ec5
Ignacio Lopez Visedo <ignacio.lopezv@santalucia.es>
2021-03-01 07:13:29 +0100

    chore: intermediate commit

--- src/main/java/com/santalucia/example/Application.java
+++ src/main/java/com/santalucia/example/Application.java
@@ -1,13 +1,17 @@
 package com.santalucia.example;
 
+import org.openapitools.configuration.HomeController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"org.openapitools.configuration"})
+@Import({HomeController.class})
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+	
 }
