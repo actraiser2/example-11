368858d487eee780a9a50e372291c0e46266119b
Duque Boj <enrique.duque.boj@accenture.com>
2021-11-03 13:16:00 +0100

    chore: Add responseEntity for SSE restController

--- src/main/java/com/santalucia/example/controller/ProgressController.java
+++ src/main/java/com/santalucia/example/controller/ProgressController.java
@@ -4,21 +4,31 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Random;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+/**
+ * Controler para ServerSentEvents
+ *
+ */
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ProgressController {
 
+	/**
+	 * @return
+	 * @throws IOException
+	 */
 	@GetMapping("/getData")
-	public SseEmitter getData() throws IOException {
+	public ResponseEntity<SseEmitter> getData() throws IOException {
 		SseEmitter sseEmitter = new SseEmitter(Long.MAX_VALUE);
 		sseEmitter.send(SseEmitter.event().name("timestamp")
 				.data(LocalDate.now() + " Nº del 1-100: " + (new Random().nextInt(100) ) ) );
-		return sseEmitter;
+		return new ResponseEntity<>(sseEmitter, HttpStatus.OK);
 	}
 
 }
