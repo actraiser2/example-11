a93b866cc7d80494fa8adbb166cdb9d8ed55e694
SANTALUCIA\25717136P <josemiguel.besadaj@santalucia.es>
2025-08-24 13:44:19 +0200

    chore: fix kiuwan

--- src/main/java/com/santalucia/example/core/mappers/DocumentResourceMapper.java
+++ src/main/java/com/santalucia/example/core/mappers/DocumentResourceMapper.java
@@ -8,5 +8,9 @@ import com.santalucia.example.api.model.DocumentoResource;
 @Mapper
 public interface DocumentResourceMapper {
 
+	/**
+	 * @param documentResource
+	 * @return
+	 */
 	DocumentoResource toDocumentoResource(com.santalucia.arq.ams.api.model.DocumentoResource documentResource);
 }
