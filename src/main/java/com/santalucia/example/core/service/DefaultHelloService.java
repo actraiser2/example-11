8d6671e0cd5d27afc86014209fcef370ce5282d1
Ignacio Lopez Visedo <ignacio.lopezv@santalucia.es>
2025-09-17 11:40:29 +0200

    feat:  Adds internationalization for hello message
    Configures the application to retrieve the hello message from message bundles.
    
    This change allows for easy translation of the greeting message to different languages, improving the user experience for international users.
    It introduces new message files for Catalan and Valencian.

--- src/main/java/com/santalucia/example/core/service/DefaultHelloService.java
+++ src/main/java/com/santalucia/example/core/service/DefaultHelloService.java
@@ -2,8 +2,7 @@ package com.santalucia.example.core.service;
 
 import java.util.Optional;
 
-
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Service;
 
 import com.santalucia.example.api.model.IdentidadDigitalConsultaResource;
@@ -11,14 +10,20 @@ import com.santalucia.example.core.domain.IdentidadDigitalDomain;
 import com.santalucia.example.core.exceptions.InvalidNameException;
 import com.santalucia.example.core.mappers.IdentidadDigitalDomainMapper;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 
 /**
  * DefaultHelloService
  *
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DefaultHelloService implements HelloService {
+	
+	private final MessageSourceAccessor messageSourceAccessor;
 
 
 	private final IdentidadDigitalDomainMapper identidadDigitalMapper;
@@ -46,11 +51,13 @@ public class DefaultHelloService implements HelloService {
 	 * @param name
 	 * @return String
 	 */
+
 	@Override
 	public IdentidadDigitalDomain getHelloByName(String name) {
+		log.error("entro por el async!!!!!!!");
 		return IdentidadDigitalDomain.builder()
 		.nombre(name)
-		.saludo("Hello %s".formatted(name))
+		.saludo(messageSourceAccessor.getMessage("label.error.407", new String[] {name}))
 		.build();
 	}
 }
